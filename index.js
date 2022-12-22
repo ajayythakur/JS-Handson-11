41bcb8936fabc9c9d82cf236ff1eb08d8b9d570a
ajayythakur <ajay1270panwar@gmail.com>
2022-12-22 19:47:33 +0530

    DOM Hansdson

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ bttn.addEventListener("click", replace);
 function replace(){
     document.getElementById("txt").innerText="Welcome to Elevation Academy";
 }
+
+let btn=document.getElementById("btn");
+btn=addEventListener("click",chdir);
+function chdir(){
+document.getElementById("container").style.flexDirection="column";
+}
